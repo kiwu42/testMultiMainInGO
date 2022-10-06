263b7c8e55a524d9b67d926187dcdd408e76ec5a
kuaniwu <2021lisag16@gmail.com>
2022-10-06 20:39:47 +0800

    init

---
+++ main/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	filename := "main.go"
+	fmt.Println("Hi I'm main() from", filename)
+}
---
+++ main/main2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	filename := "main2.go"
+	fmt.Println("Hi I'm main() from", filename)
+}
